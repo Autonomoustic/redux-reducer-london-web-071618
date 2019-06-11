db0decea99e54f5e86d16c6abe3373759c3e91db
ali aziz <aa_connect@outlook.com>
2019-06-11 12:08:15 +0000

    Automatically backed up by Learn

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,9 +1,8 @@
-export function managePresents (action, state) {
+export function managePresents (state, action) {
   switch (action.type) {
     case 'INCREASE':
       return state + 1
     default:
       return state
-      
   }
 }
