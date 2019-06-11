c715553d2d96531f4baabf0c83520caee9511953
ali aziz <aa_connect@outlook.com>
2019-06-11 12:21:59 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -3,7 +3,7 @@ export function manageFriends (state, action) {
   console.log(action)
   switch (action.type) {
     case 'ADD_FRIEND':
-      return { friends: {...state.friends} }
+      return { friends: {...state.friends, ...action.friend} }
     default :
       return state
   }
